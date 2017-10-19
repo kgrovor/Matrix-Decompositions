553d3aae177fb1f5fa8c3175eedf46602732da03
Kshitij <kgrovor@gmail.com>
2017-10-19 10:31:20 +0530

    comments

--- data.py
+++ data.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse as sps
+
 #column headers for the dataset
 data_cols = ['user_id','movie_id','rating','timestamp']
 item_cols = ['movie id','movie title','release date','video release date','IMDb URL','unknown','Action','Adventure','Animation','Childrens','Comedy','Crime','Documentary','Drama','Fantasy','Film-Noir','Horror','Musical','Mystery','Romance ','Sci-Fi','Thriller','War' ,'Western']
@@ -19,10 +20,11 @@ users = pd.read_csv('ml-100k/u.user', sep='|',names=user_cols, encoding='latin-1
 item = pd.read_csv('ml-100k/u.item', sep='|',names=item_cols, encoding='latin-1')
 data = pd.read_csv('ml-100k/u.data', sep='\t',names=data_cols, encoding='latin-1')
 
-    
+#==============================================================================
+#  Creating the Adjacency matrix M with rows as users and columns as movies   
+#==============================================================================
 df = pd.crosstab(data.user_id,data.movie_id,values=data.rating,aggfunc="mean")
 df.fillna(value=0,inplace=True)
-#print(df.head())
-
 M = sps.coo_matrix((df))
 del df
+
