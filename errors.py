06daf1613b096c2395724d192e89cae243f0c416
Bhavathi <bhavathireddy@gmail.com>
2017-11-14 10:43:38 +0530

    xyz

--- errors.py
+++ errors.py
@@ -6,14 +6,13 @@ Created on Mon Nov 13 15:26:38 2017
 @author: bhavathi
 """
 import numpy as np
-from scipy.stats import spearmanr as spearm
 import test
 
 def rmse(predictions, targets):
     return np.sqrt(((predictions - targets) ** 2).mean())
 
 def spear(predictions, targets):
-    return 1-(6*np.sum((predictions - targets) ** 2)/len(predictions)/(len(predictions)**2 - 1))
+    return np.mean(1-(6*np.sum(np.square(predictions - targets),axis=1)/predictions.shape[1]/((predictions.shape[1])**2 - 1)))
 
 def calc_error(M):
     M_test=np.matrix(test.M.todense())
@@ -22,17 +21,19 @@ def calc_error(M):
     predicted=np.squeeze(np.asarray(M[np.nonzero(M_test)]))
 
     rms=rmse(predicted, actual)
-    spm=spearm(actual,predicted)
+    M[M_test==0]=0
+    Pred=np.argsort(-M)
+    Act=np.argsort(-M_test)
+
+    spm=spear(Act,Pred)
     print ("RMSE : ", rms)
 #    print(spear(predicted,actual))
-    print("Spearman Correlation Coefficient: ",spm[0])
+    print("Spearman Correlation Coefficient : ",spm*100)
     
-    M[M_test==0]=0
     k=100
     precision=0.0
-    Pred=np.argsort(-M)[:,:k]
-    Act=np.argsort(-M_test)[:,:k]
-
+    Pred=Pred[:,:k]
+    Act=Act[:,:k]
     for i in range(Pred.shape[0]):
         for j in Pred[i,:]:
             if (j in Act[i,:]):
@@ -41,5 +42,3 @@ def calc_error(M):
     precision=precision/Pred.shape[0]
     print("Precision on top ",k," : ",precision*100,"%")
     return 0
-
-#calc_error (data.M.todense())
